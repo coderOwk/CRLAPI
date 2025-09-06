245123bd6a4db76e3b09883e47c638bad3850676
coderOwk <141489271+coderOwk@users.noreply.github.com>
2025-09-06 13:06:57 +0300

    added key

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const app = express();
 
 // your Clash Royale token here:
-const TOKEN = "YOUR_SUPERCELL_API_TOKEN_HERE";
+const TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY2MGJjMTRmLWVmNWQtNDQ5Mi04YTQ0LWM0NTEwMzYwNGFjZiIsImlhdCI6MTc1NjgxMjUzMywic3ViIjoiZGV2ZWxvcGVyLzMyZTMxNzBjLTg1NmEtYjBkMC02ZWIyLWY2YjU4YTcwYTZlYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzYuMjM0LjguMTc4Il0sInR5cGUiOiJjbGllbnQifV19.I_d3iM3f975DkVt3ncUE0zYlbkvEWUEY7QTi_yXjZl8siOlvBd9YR68IA2rLViEHfAuM4v4WyBV8fP--Cev0TA";
 
 app.get("/player/:id", async (req, res) => {
   const playerId = req.params.id;
